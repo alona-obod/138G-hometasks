28f6c9220053cef7776b74a6e9c59f9f2343b523
alona <alona.obod@gmail.com>
2021-11-12 14:49:22 +0200

    Changes

---
+++ Tasks/Count.py
@@ -0,0 +1,21 @@
+print (555 + 444)
+print (33 - 22)
+print (888 / 20)
+print (3 * 333)
+
+a = 88
+b = 103
+print (a + b)
+print (a - b)
+print (a / b)
+print (a * b)
+
+a = 88
+b = 103
+c = 52
+print ((a * b) / c)
+print ((a - b) * c)
+print (a + b - c)
+print (a / b * c)
+
+
